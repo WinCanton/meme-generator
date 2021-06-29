286d9c9553680fc4b0f6986dcc4bd994e019d499
Martin <martinms@gmail.com>
2021-06-29 23:23:08 +0100

    Completed

---
+++ src/Readme.md
@@ -0,0 +1,7 @@
+MEME GENERATOR
+
+This application supports the use of CLI (Command Line Interface) and the Web Interface to generate random memes by using image and quotes.
+
+To get help on how to use the application using Command Line interface, please use `python meme.py --help` command.
+
+Please see requirements.txt file for python modules required for the application to run.
--- src/app.py
+++ src/app.py
@@ -2,8 +2,8 @@ import random
 import os
 import requests
 from flask import Flask, render_template, abort, request
-
-# @TODO Import your Ingestor and MemeEngine classes
+from QuoteEngine import Ingestor, QuoteModel
+from MemeGenerator import MemeEngine
 
 app = Flask(__name__)
 
@@ -18,15 +18,16 @@ def setup():
                    './_data/DogQuotes/DogQuotesPDF.pdf',
                    './_data/DogQuotes/DogQuotesCSV.csv']
 
-    # TODO: Use the Ingestor class to parse all files in the
-    # quote_files variable
-    quotes = None
+    quotes = []
+
+    for file in quote_files:
+        quotes.extend(Ingestor.parse(file))
 
     images_path = "./_data/photos/dog/"
 
-    # TODO: Use the pythons standard library os class to find all
-    # images within the images images_path directory
-    imgs = None
+    imgs = []
+    for root, dirs, files in os.walk(images_path):
+        imgs = [os.path.join(root, name) for name in files]
 
     return quotes, imgs
 
@@ -38,13 +39,8 @@ quotes, imgs = setup()
 def meme_rand():
     """ Generate a random meme """
 
-    # @TODO:
-    # Use the random python standard library class to:
-    # 1. select a random image from imgs array
-    # 2. select a random quote from the quotes array
-
-    img = None
-    quote = None
+    img = random.choice(imgs)
+    quote = random.choice(quotes)
     path = meme.make_meme(img, quote.body, quote.author)
     return render_template('meme.html', path=path)
 
@@ -59,14 +55,15 @@ def meme_form():
 def meme_post():
     """ Create a user defined meme """
 
-    # @TODO:
-    # 1. Use requests to save the image from the image_url
-    #    form param to a temp local file.
-    # 2. Use the meme object to generate a meme using this temp
-    #    file and the body and author form paramaters.
-    # 3. Remove the temporary saved image.
+    image_url = requests.form['image_url']
+    body = requests.form['body']
+    author = requests.form['author']
 
-    path = None
+    image = requests.get(image_url)
+    output_file = f'./static/{random.randint(0,10000000)}.png'
+    open(output_file, 'wb').write(image.content)
+
+    path = meme.make_meme(output_file, body, author)
 
     return render_template('meme.html', path=path)
 
